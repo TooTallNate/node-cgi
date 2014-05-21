bea8ab6526fb5413a49089afbd6310c15269c735
Nathan Rajlich <nathan@tootallnate.net>
2014-05-21 11:13:04 -0700

    test: fix "env" test

--- test/env.js
+++ test/env.js
@@ -1,23 +1,27 @@
 var http = require('http');
 var cgi  = require('../cgi');
-var stack = require('stack');
+var connect = require('connect');
 
 var PORT = 5555;
-var server = http.createServer(
-  stack(
-    require('creationix/log')(),
-    cgi(__dirname + '/cgi-bin/printenv.cgi')
-  )
-);
+var app = connect()
+  .use(connect.logger())
+  .use(cgi(__dirname + '/cgi-bin/printenv.cgi'));
+
+var server = http.createServer(app);
+
 server.listen(PORT, function() {
   console.log('server listening');
 
-  var client = http.createClient(PORT);
-  var req = client.request('GET', '/?test=1');
+  var req = http.get({
+    port: PORT,
+    path: '/?test=1'
+  });
+
   req.on('response', function (res) {
     console.log(res.headers);
-    res.pipe(process.stdout, {end:false});
-    res.on('end', client.end.bind(client));
+    res.pipe(process.stdout);
+    res.on('end', server.close.bind(server));
   });
+
   req.end();
 });
